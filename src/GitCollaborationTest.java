911c268d6e1f9a687e3754498ea456fcf0bea091
chengcj-upenn <chengcj@seas.upenn.edu>
2019-04-10 22:22:04 -0700

    added constructor for Welcome class

--- src/GitCollaborationTest.java
+++ src/GitCollaborationTest.java
@@ -1,15 +1,16 @@
 import java.util.ArrayList;
 
 public class GitCollaborationTest {
-	
+
 	public static void main(String[] args) {
 		ArrayList<Welcome> welcomeArray = new ArrayList<Welcome>();
 
-		Welcome myGreeting = new Welcome();
-		myGreeting.type = "Welcome to Github!!!";
-		myGreeting.yourName = "Chin Jung Cheng";
+		String type = "Welcome to Github!!!";
+		String yourName = "Chin Jung Cheng";
+		Welcome myGreeting = new Welcome(type, yourName);
+
 		welcomeArray.add(myGreeting);
-		
+
 		for (Welcome greeting : welcomeArray) {
 			System.out.println(greeting.type + '\n' + greeting.yourName);
 		}
--- src/Welcome.java
+++ src/Welcome.java
@@ -2,5 +2,9 @@
 public class Welcome {
 	String type;
 	String yourName;
-
+	
+	Welcome(String type, String yourName){
+		this.type = type;
+		this.yourName = yourName;
+	}
 }
